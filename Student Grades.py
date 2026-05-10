80f3a0c21a1d58c36bae231a9fd875b4610be46a
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-10 01:20:25 -0700

    Create Student Grades.py

---
+++ Student Grades.py
@@ -0,0 +1,10 @@
+marks = int(input("enter yours marks"));
+if marks >= 80:
+    print("A grade");
+elif marks >= 70 and  marks< 80:
+    print("B grade");
+elif marks >= 60 and marks < 70:
+    print("C grade")
+else:
+    print("F grade")
+
