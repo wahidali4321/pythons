4dd0be11084e6a5dabaaff754956ca7caf21b446
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-15 08:56:09 +0500

    Create nested_if.py

---
+++ nested_if.py
@@ -0,0 +1,7 @@
+number = 41 ;
+if number > 10 :
+    print("number is greater than 10");
+if number > 20 :
+    print("number is greater than 20") ;
+else:
+    print("number is not greter than 20")
