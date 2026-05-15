5f31cb5c117158748bff8f44de629394e9cb60f2
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-15 10:49:38 +0500

    Create nested_if4.py

---
+++ nested_if4.py
@@ -0,0 +1,13 @@
+score = 85 ;
+attendence = 90 ;
+submitted = True ;
+
+if score >= 60 :
+    if attendence >= 80 :
+        print("passing with good marks")
+    else:
+        print("pass but missing assigment")
+    else:
+    print("pass but low attendence")
+else:
+    print("fail")
