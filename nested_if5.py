05bc178c89c1842d517fa0dcb8c8f2eae7751ddf
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-15 10:50:51 +0500

    Create nested_if5.py

---
+++ nested_if5.py
@@ -0,0 +1,14 @@
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+    
+    if attendance >= 80:
+        print("Passing with good marks")
+    
+    else:
+        print("Pass but low attendance")
+
+else:
+    print("Fail")
