7dc83d9869f23b627c4129a679ad5fe6e965f2b5
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-15 09:09:03 +0500

    Create nested_if2.py

---
+++ nested_if2.py
@@ -0,0 +1,9 @@
+age = 20 ;
+has_licensed = True 
+if age >= 24 :
+    if has_licensed :
+        print("you can drive")
+    else :
+        print("need a licensed");
+else :
+    print("you are too young for drive")
