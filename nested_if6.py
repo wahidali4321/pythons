6ffcafb544a7b5bb5c06a9929171d777192fe131
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-15 12:47:44 +0500

    Create nested_if6.py

---
+++ nested_if6.py
@@ -0,0 +1,14 @@
+username = "Emil"
+password = "python123"
+is_active = True
+
+if username:
+  if password:
+    if is_active:
+      print("Login successful")
+    else:
+      print("Account is not active")
+  else:
+    print("Password required")
+else:
+  print("Username required")
