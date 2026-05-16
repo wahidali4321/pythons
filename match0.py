0d6f419b640e21d5e8b9072c6c8e55c4a9b687b3
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-16 12:19:35 +0500

    Create match0.py

---
+++ match0.py
@@ -0,0 +1,16 @@
+day = 2 ;
+match day:
+    case 1 :
+        print("monday")
+    case 2 :
+        print("tuesday")
+    case 3 :
+        print("wednesday")
+    case 4 :
+        print("thursday")
+    case 5 :
+        print("friday")
+    case 6 :
+        print("saturday")
+    case 7 :
+        print("sunday")
