b969ea82fedad1be2a39d45ac7df32366ac9fcf8
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-17 15:54:59 +0500

    updated

---
+++ Secret Password System0.py
@@ -0,0 +1,7 @@
+password = "python"
+
+while True :
+    confirmed = str(input("enter the password"))
+    if confirmed == password :
+        break
+    print("permission granted")
---
+++ Secret Password System1.py
@@ -0,0 +1,9 @@
+password = "python"
+
+while True:
+    
+    confirmed = input("Enter the password: ")
+
+    if confirmed == password:
+        print("Access Granted")
+        break
