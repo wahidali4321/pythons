35197d4603f81217722dbf67cda41e88eb563d2d
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-19 15:57:54 +0500

    updates

---
+++ multiplication1 .py
@@ -0,0 +1,10 @@
+def multiplication(number) :
+    return number * 3
+
+number = int(input("enter the number : "))
+
+while number < 10 :
+    number += 1
+
+number = int(input("enter the number : "))
+print(multiplication(number))
