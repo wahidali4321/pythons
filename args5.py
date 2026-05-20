ca52017e56818a03895243aa8d777d88fbef0b4e
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-20 11:19:59 +0500

    updated

---
+++ args5.py
@@ -0,0 +1,6 @@
+def myFunction(*args) :
+    print("Types of : " , type(args))
+    print("My name is  : " , args[0])
+    print("My father name is : " , args[1])
+    print("My brother name is : " , args[2])
+myFunction("wahid " , "Ali" , "wazir")
