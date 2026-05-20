5543af5713480520d366353c3bc8468df8bcdd31
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-20 11:15:37 +0500

    updated

---
+++ args4.py
@@ -0,0 +1,7 @@
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
