16f5eb1bd3ff4866eaa71aede786159fd5623540
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-20 11:31:08 +0500

    updated

---
+++ maximum value.py
@@ -0,0 +1,10 @@
+def my_function(*numbers):
+  if len(numbers) == 0:
+    return None
+  max_num = numbers[0]
+  for num in numbers:
+    if num > max_num:
+      max_num = num
+  return max_num
+
+print(my_function(3, 7, 2, 9, 1))
