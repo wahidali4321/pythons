dc7da28fde065c6af90fcbd89f36d88dda0ac34f
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-05-25 14:25:40 +0500

    decorter

---
+++ def changecase0.py
@@ -0,0 +1,10 @@
+def changcase(func) :
+    def myinner() :
+        return func().upper()
+    return myinner()
+
+@changcase
+def myFunction() :
+    return "Hello wahid ali"
+
+print(myFunction())
